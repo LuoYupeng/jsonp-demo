9c869d45bcc7623d75f90d3686bd1345f3353bf5
luoyupeng <461935859@qq.com>
2019-03-27 01:03:57 +0800

    jQuery写JSONP

--- main.js
+++ main.js
@@ -1,30 +1,45 @@
 button.addEventListener('click', (e)=>{
 
-        let script = document.createElement('script')
-        let functionName = 'tom' + parseInt(Math.random() * 10000, 10)
-
-        window[functionName] = function (result) {
-
-            if (result === 'success') {
-                
+    //jQuery写jsonp
+    $.ajax({
+        url: 'http://jerry.com:8002/pay',
+        dataType: 'jsonp',
+        success: function (response) {
+            if(response === 'success'){
                 amount.innerText = amount.innerText - 1
-
-            } else {
-
             }
         }
-        script.src = 'http://jerry.com:8002/pay?callback=' + functionName
-        document.body.appendChild(script)
-        script.onload = function (e) {
-            e.currentTarget.remove()
-            delete window[functionName]
-        }
+    })
 
-        script.onerror = function () {
-            alert('fail')
-            e.currentTarget.remove()
-            delete window[functionName]
-        }
+
+
+
+    // jsonp运行机理
+    // let script = document.createElement('script')
+    // let functionName = 'tom' + parseInt(Math.random() * 10000, 10)
+    //
+    // window[functionName] = function (result) {
+    //
+    //     if (result === 'success') {
+    //
+    //         amount.innerText = amount.innerText - 1
+    //
+    //     } else {
+    //
+    //     }
+    // }
+    // script.src = 'http://jerry.com:8002/pay?callback=' + functionName
+    // document.body.appendChild(script)
+    // script.onload = function (e) {
+    //     e.currentTarget.remove()
+    //     delete window[functionName]
+    // }
+    //
+    // script.onerror = function () {
+    //     alert('fail')
+    //     e.currentTarget.remove()
+    //     delete window[functionName]
+    // }
 
 
 
